c1afb012c67cc00dd3a128613deec439ecaf670c
Maldeojos <tiansanchez2000@gmail.com>
2026-04-22 01:14:09 -0400

    Se añadieron casos de prueba de la lista para mostrar al inicion de la app, ademas se dejo un comentario para preguntar en clases

--- app/src/main/java/com/example/event_master/components/FormViewModel.kt
+++ app/src/main/java/com/example/event_master/components/FormViewModel.kt
@@ -10,7 +10,11 @@ class CategoriaViewMdole: ViewModel(){
     var nombre by mutableStateOf("")
     var descripcion by mutableStateOf("")
     // var color by mutableStateOf("")  Preguntar profesor como tomar color para las cards
-    var listEvento = mutableListOf<Actividad>()
+    var listEvento = mutableListOf<Actividad>(
+        Actividad("Musica","Concierto","Banda local"),
+        Actividad("Deporte","Partido Futbol","5pm, San Pedro de la paz"),
+        Actividad("Cultura","Feria del libro","Biblioteca UCSC, 20:30 horas")
+    )
     fun addEvento(){
         listEvento.add(Actividad(tipo,nombre,descripcion))
     }
@@ -18,3 +22,10 @@ class CategoriaViewMdole: ViewModel(){
 }
 
 class Actividad(val tipo: String,val nombre: String,val descripcion: String)
+
+
+// Consultar al profe viavilidad, Crear una clase Actividad que sea de tipo lista e id(automatica),
+// en la cual se guarden las actividades (Id, Nombre, Descripcion).
+// Los tipos de eventos se mostraran en el inicio como tarjetas, con una lista (tarjetas) de las
+// actividades, al clickear o seleccionar una de las actividades desplegara informacion o se
+// avanzara a la pagina descriptiva
