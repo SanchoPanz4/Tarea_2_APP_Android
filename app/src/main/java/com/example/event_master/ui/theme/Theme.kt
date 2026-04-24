35af28db0dbead9a28cce390c9d5eb53db841edc
zanelf <63268578+zanelf@users.noreply.github.com>
2026-04-23 22:15:41 -0400

    version final

--- app/src/main/java/com/example/event_master/ui/theme/Theme.kt
+++ app/src/main/java/com/example/event_master/ui/theme/Theme.kt
@@ -93,7 +93,7 @@ private val darkScheme = darkColorScheme(
 fun Event_MasterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
